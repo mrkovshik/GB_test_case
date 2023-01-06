e023c984c553b5f7a7db4fbe4498d29fa76cf0f3
kolya <kovshik@inbox.ru>
2023-01-06 12:07:36 +0300

    разнес код по функциям еще больше, прикрутил флаги

--- README.md
+++ README.md
@@ -1,26 +1,27 @@
 # GB_test_case
-test exercise for the geek brains project
+## тестовое задание для проекта Geek Brains
 
 Код написан на Go для базы данных PostgreSQL. 
 Для начала работы назначьте константам host, port, user, password, dbname значения, соответствующие вашей базе данных.
 
 Для удобства ниже указан запрос для создания таблиц в PostgreSQL:
 
+```
+CREATE TABLE job_types (id SERIAL PRIMARY KEY,
+                                          job_type text NOT NULL);
 
 
-CREATE TABLE job_types (
-    id SERIAL PRIMARY KEY,
-    job_type text NOT NULL
-);
+CREATE TABLE vacancies (id SERIAL PRIMARY KEY,
+                                          vacancy_name text NOT NULL,
+                                                            key_skills text NOT NULL,
+                                                                            vacancy_desc text NOT NULL,
+                                                                                              salary integer NOT NULL,
+                                                                                                             job_type integer REFERENCES job_types(id));
 
-CREATE TABLE vacancies (
-    id SERIAL PRIMARY KEY,
-  vacancy_name text NOT NULL,
-  key_skills text NOT NULL,
-  vacancy_desc text NOT NULL,
-  salary integer NOT NULL, 
-  job_type integer references job_types(id)
-);
 
-Insert into job_types (job_type)
-values ('В офисе'), ('Удаленно'), ('Гибридный');
+INSERT INTO job_types (job_type)
+VALUES ('В офисе'),
+       ('Удаленно'),
+       ('Гибридный');```
+
+       разделить функции на смысловые блоки, вынести подключение к базе в мэйн, добавить пинг дб перед выполнение функции, обращающейся к дб. переделать подключение к базе через флаги. создать миграцию и положить в репу. Миграция для создания бд, create if not exist database, switch toda
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,23 +12,32 @@ import (
 
 	_ "github.com/lib/pq"
 )
-
-var showQry string = " SELECT vacancies.id, vacancy_name, key_skills, salary, vacancy_desc, job_types.job_type FROM vacancies JOIN job_types ON vacancies.job_type = job_types.id;"
+var mainMenu string = "\n*****************************\n - Если хотите посмотреть всю таблицу вакансий, наберите \"посмотреть\", \n - Если хотите найти вакансию по названию наберите \"найти\"\n - Если хотите добавить строку - наберите \"добавить\", \n - Если хотите выйти из программы, наберите \"выход\"\n*****************************\n"
 var searchQry string = " SELECT vacancies.id, vacancy_name, key_skills, salary, vacancy_desc, job_types.job_type FROM vacancies JOIN job_types ON vacancies.job_type = job_types.id WHERE vacancy_name ILIKE '%"
+var db *sql.DB
+type dbCred struct{
+	host string
+	 port int
+	  user string
+	   password string
+	    dbName string
+}
 
-const (
-	host     = "172.19.97.209" //впишите сюда параметры своей базы данных
-	port     = 5432
-	user     = "postgres"
-	password = "17pasHres19!"
-	dbname   = "vacancies"
-)
+type vacQery struct {
+	vacName string
+	keySkills string
+	vacDesc string
+	salary int
+	jobCode int
+}
 
-func connectDB() *sql.DB {
+func connectDB(cred dbCred) (*sql.DB, error) {
+
 	// Connect to the database
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+		cred.host, cred.port, cred.user, cred.password, cred.dbName)
+		fmt.Println(psqlInfo)
 
 	db, err := sql.Open("postgres", psqlInfo)
 
@@ -35,67 +45,66 @@ func connectDB() *sql.DB {
 		log.Fatal(err)
 	}
 
-	return db
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db,err
 }
 
-func showVacs(qry string) {
-
-	if qry != showQry {
-		fmt.Println("Введите название вакансии частично или полностью, либо наберите \"назад\" для выхода в предыдущее меню")
-		scanner := bufio.NewScanner(os.Stdin)
-		for {
-			fmt.Print("> ")
-			if scanner.Scan() {
-				if scanner.Text() == "назад" {
-					return
-				}
-				qry = searchQry + scanner.Text() + "%';"
-				break
+func searchDialog () (string, bool){
+	fmt.Println("Введите название вакансии частично или полностью, либо наберите \"назад\" для выхода в предыдущее меню")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		if scanner.Scan() {
+			if scanner.Text() == "назад" {
+				return "",false
 			}
+			searchKey:=scanner.Text()
+			return searchKey,true
 		}
 	}
-	db := connectDB()
-	defer db.Close()
+}
+
+func showVacs(qry string, db *sql.DB) error {
 	rows, err := db.Query(qry)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 	defer rows.Close()
 	counter:=0
-	headerPrinted:=false
 	for rows.Next() {
 		counter++
 		var id, salary int
 		var vacancy_name, key_skills, vacancy_desc, job_type string
 		err = rows.Scan(&id, &vacancy_name, &key_skills, &salary, &vacancy_desc, &job_type)
 		if err != nil {
-			panic(err.Error())
+			return err
 		}
-		if counter>0&&!headerPrinted{
+		if counter==1{
 			fmt.Println("ID    |   Vacancy Name   |  Key Skills                                          |  Vacancy Description                                                     |  Salary    |  Job Type")
 		fmt.Println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-		headerPrinted=true
+		
 	}
 		fmt.Printf("%-4v  |  %-14v  |  %-50v  |  %-70v  |  %-8v  |  %-10v\n", id, vacancy_name, key_skills, vacancy_desc, salary, job_type)
 	}
 	if counter == 0 {
 		fmt.Println("\nПохоже, по такому запросу в базе ничего не нашлось. Попробуйте изменить запрос")
 		fmt.Println("----------------------------------------")
-		return
+		return err
 	}
 	err = rows.Err()
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
+	return err
 }
-
-func insert() {
-	var queryString []string
-	var sal, jobCode int
+func insertDialog() (vacQery, bool){
+	var result vacQery
 	var err error
-
 	fmt.Println("введите соответствующие значения строк, разделяя их знаком \"/\": ")
-	fmt.Println("название вакансии, ключевые навыки, описание вакансии, зарплата, и код типа работы: 1 для работы в офисе, 2 для удаленной работы и 3 для гибридной")
+	fmt.Println("название вакансии, ключевые навыки, описание вакансии, зарплата, и код типа работы: 1 для работы в офисе, 2 для удаленной работы и 3 для гибридной формы работы")
 	fmt.Println("Например: \"Охранник/Решать конфликтные ситуации, обращаться с оружием, разгадывать сканворды/Человек, который следит за порядком в офисном здании/50000/1\"")
 	fmt.Println("или наберите \"назад\" для выхода в предыдущее меню")
 	scanner := bufio.NewScanner(os.Stdin)
@@ -103,51 +112,56 @@ func insert() {
 		fmt.Print("> ")
 		if scanner.Scan() {
 			if scanner.Text() == "назад" {
-				return
+				return vacQery{},false
 			}
 
-			queryString = strings.Split(scanner.Text(), "/")
+			queryString:= strings.Split(scanner.Text(), "/")
 			if len(queryString) != 5 {
 				fmt.Println("Неверное количество аргументов, повторите ввод")
 				continue
 			}
-			sal, err = strconv.Atoi(queryString[3])
+			result.vacName=queryString[0]
+			result.keySkills=queryString[1]
+			result.vacDesc=queryString[2]
+			result.salary, err = strconv.Atoi(queryString[3])
 			if err != nil {
 				fmt.Println("Ошибка ввода данных в поле \"Зарплата\", повторите ввод")
 				continue
 			}
-			jobCode, err = strconv.Atoi(queryString[4])
+			result.jobCode, err = strconv.Atoi(queryString[4])
 			if err != nil {
 				fmt.Println("Ошибка ввода данных в поле \"код типа работы\", повторите ввод")
 				continue
 			}
+			if result.jobCode>3|| result.jobCode<1{
+				fmt.Println("Код работы может быть только следующих значений: 1 для работы в офисе, 2 для удаленной работы и 3 для гибридной формы работы. Ввод других значений не допускается")
+				continue
+			}
+			}
+			
+			return result,true
 
-		}
-		db := connectDB()
-		defer db.Close()
-		// Prepare the INSERT statement
+	}
+	}
+func insert(q vacQery, db *sql.DB) error {
+fmt.Println(q)
 		stmt, err := db.Prepare("INSERT INTO vacancies (vacancy_name,key_skills, vacancy_desc ,  salary, job_type) VALUES($1, $2,$3,$4,$5)")
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
-		defer stmt.Close()
-
-		// Execute the INSERT statement
-		_, err = stmt.Exec(queryString[0], queryString[1], queryString[2], sal, jobCode)
+		_, err = stmt.Exec(q.vacName, q.keySkills, q.vacDesc, q.salary, q.jobCode)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
+		stmt.Close()
 		fmt.Println("Запись добавлена")
-		return
+		return nil
 	}
-}
 
-func main() {
-	var mainMenu string = "\n*****************************\n - Если хотите посмотреть всю таблицу вакансий, наберите \"посмотреть\", \n - Если хотите найти вакансию по названию наберите \"найти\"\n - Если хотите добавить строку - наберите \"добавить\", \n - Если хотите выйти из программы, наберите \"выход\"\n*****************************\n"
-	scanner := bufio.NewScanner(os.Stdin)
+ func mainDialog () error {
 	fmt.Println(mainMenu)
-	// Loop indefinitely
-OuterLoop:
+	scanner := bufio.NewScanner(os.Stdin)
+	OuterLoop:
 	for {
 		// Print a prompt
 		fmt.Print("> ")
@@ -156,13 +170,31 @@ OuterLoop:
 		if scanner.Scan() {
 			switch {
 			case scanner.Text() == "посмотреть":
-				showVacs(showQry)
+				err:=showVacs(searchQry+"%'",db)
+				if err!=nil{
+					fmt.Println("Ошибка обращения к базе данных",err)
+					return err
+					}
 				fmt.Println(mainMenu)
 			case scanner.Text() == "найти":
-				showVacs("")
+				keyWord, proceed:=searchDialog()
+				if proceed{
+				err:=showVacs(searchQry+keyWord+"%'",db)
+				if err!=nil{
+					fmt.Println("Ошибка обращения к базе данных",err)
+					return err
+					}
+				}
 				fmt.Println(mainMenu)
 			case scanner.Text() == "добавить":
-				insert()
+				query,proceed:=insertDialog()
+				if proceed{
+				err:=insert(query,db)
+				if err!=nil {
+					fmt.Println("Ошибка внесения данных в таблицу",err)
+					return err
+				}
+				}
 				fmt.Println(mainMenu)
 			case scanner.Text() == "выход":
 				fmt.Println("Всего хорошего!")
@@ -171,4 +203,25 @@ OuterLoop:
 			}
 		}
 	}
+	return nil
+ }
+func main() {
+	var err error
+	var cred dbCred
+	flag.IntVar(&cred.port, "port", 5432, "Port for DB connection")
+	flag.StringVar(&cred.host, "h", "172.19.97.209", "DB host IP")
+	flag.StringVar(&cred.password, "p", "17pasHres19!", "DB connection password")
+	flag.StringVar(&cred.user, "u", "postgres", "DB connection user name")
+	flag.StringVar(&cred.dbName, "n", "vacancies", "DB name")
+	
+	db,err=connectDB(cred)
+	if err!=nil{
+		fmt.Println("Ошибка подключения базы данных", err)
+		return 
+	}
+	defer db.Close()
+	mainDialog ()
+
+
+
 }
